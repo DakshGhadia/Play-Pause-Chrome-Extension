aefb578fc2934245010a6ec6b885babe929f8542
Daksh <ghadiadaksh@gmail.com>
2024-08-19 09:14:07 -0400

    Added functionality of when to pause and when to resume

--- dist/js/background.js
+++ dist/js/background.js
@@ -1,5 +1,21 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((message) => {
   if (message.isChecked) {
-    console.log("Toggle state changed:", message.isChecked);
+    chrome.windows.onFocusChanged.addListener((windowId) => {
+      if (windowId === chrome.windows.WINDOW_ID_NONE) {
+        // Window lost focus
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          if (tabs[0]) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "pauseVideo" });
+          }
+        });
+      } else {
+        // Window gained focus
+        chrome.tabs.query({ active: true, windowId: windowId }, (tabs) => {
+          if (tabs[0]) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "resumeVideo" });
+          }
+        });
+      }
+    });
   }
 });
