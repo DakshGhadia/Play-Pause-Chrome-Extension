d7ddde7ec2ba2c7bf9aca96b333e63fee0d18b51
Daksh <ghadiadaksh@gmail.com>
2024-08-19 06:54:48 -0400

    Used chrome storage to store the value of toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { useState, useEffect } from "react";
 function App() {
+  const [isChecked, setChecked] = useState(false);
+
+  useEffect(() => {
+    chrome.storage.local.get(["checked"], (result) => {
+      setChecked(result.checked ?? false);
+    });
+  }, []);
+
+  function toggleChecked() {
+    setChecked((prevChecked) => {
+      const newChecked = !prevChecked;
+      chrome.storage.local.set({ checked: newChecked }).then(() => {
+        console.log("Value is set");
+      });
+      return newChecked;
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-96 w-96 bg-gray-800 rounded-lg shadow-lg flex flex-col items-center">
+        <h1 className="text-white text-4xl font-semibold tracking-wide">
+          Video Auto Pause
+        </h1>
+        <div className="flex items-center mt-8 justify-center w-auto">
+          <label className="text-white mr-2">Auto Pause</label>
+          <label className="inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={toggleChecked}
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: [tailwindcss, autoprefixer],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "manifest.json", to: "../manifest.json" }],
+    }),
+    ...getHtmlPlugins(["index"]),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    path: path.join(__dirname, "dist/js"),
+    filename: "[name].js",
+  },
+};
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HTMLPlugin({
+        title: "React extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
